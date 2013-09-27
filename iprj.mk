e23b869c2d37724d4fac4528267f6af74826e55b
Tom Marshall <tdm.code@gmail.com>
2013-09-27 21:43:50 +0000

    Superuser daemon support

--- iprj.mk
+++ iprj.mk
@@ -71,6 +71,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Superuser
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/init.su.sh:system/etc/init.su.sh
+
 PRODUCT_PACKAGES += \
     hdmid \
     libgenlock \
